eb6b3bd4a410f4d410851b914207260122ee0e0d
Pooja Tyagi <PoojaTyagi@poojas-macbook-pro.local>
2017-03-05 13:37:38 +0530

    Initial Commit

---
+++ SimpleTableViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTableViewController
+//
+//  Created by Pooja Tyagi on 05/03/17.
+//  Copyright © 2017 Pooja Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
